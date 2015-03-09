bfd9491dfa900dab5e39a09142c04c0e5a03a575
Uni- <administrator@korea.ac.kr>
2015-03-09 16:40:52 +0900

    Because I came up with an error when I tried to use a variable named 's'

--- js.js
+++ js.js
@@ -1,15 +1,15 @@
 while (true) {
-	var s;
+	var NHqK6i6xWR$pT25xXVV6K2xzFEOcDGHTz7lwJy_Y9ymbIjETcWxli$sPxAuJ_uS;
 	WScript.StdOut.Write(":> ");
 	try {
-		s = WScript.StdIn.ReadLine();
+		NHqK6i6xWR$pT25xXVV6K2xzFEOcDGHTz7lwJy_Y9ymbIjETcWxli$sPxAuJ_uS = WScript.StdIn.ReadLine();
 	} catch (e) {
 		WScript.Echo("Read Error " + e.message);
 		continue;
 	}
 	WScript.StdOut.Write("<: ");
 	try {
-		WScript.Echo(eval(s));
+		WScript.Echo(eval(NHqK6i6xWR$pT25xXVV6K2xzFEOcDGHTz7lwJy_Y9ymbIjETcWxli$sPxAuJ_uS));
 	} catch (e) {
 		WScript.Echo("Eval Error " + e.message);
 		continue;
