f582c59280f950763a7bcbdc636a64ee9d482908
Uni- <administrator@korea.ac.kr>
2015-03-23 15:57:56 +0900

    Add a custom object-to-string dump function

--- js.js
+++ js.js
@@ -1,3 +1,32 @@
+var FV0IEh__qo76togShNc4ExIPkaagButmM17zwtGKNmh7Wjf$7lyJl$g7WGFuC0L = (function(o) {
+	var result = "";
+	if (o instanceof Array) {
+		for (var i = 0; i < o.length; i++) {
+			if (result.length) {
+				result += ",";
+			}
+			result += FV0IEh__qo76togShNc4ExIPkaagButmM17zwtGKNmh7Wjf$7lyJl$g7WGFuC0L(o[i]);
+		}
+		result = "[" + result + "]";
+	} else if (typeof o == "object") {
+		for (var i in o) {
+			if (result.length) {
+				result += ",";
+			}
+			result += "\"" + i + "\":" + FV0IEh__qo76togShNc4ExIPkaagButmM17zwtGKNmh7Wjf$7lyJl$g7WGFuC0L(o[i]);
+		}
+		result = "{" + result + "}";
+	} else if (typeof o == "string") {
+		result = new String(o).replace(/\\/g, "\\\\").replace(/\"/g, "\\\"");
+		result = "\"" + result + "\"";
+	} else if (typeof o == "undefined") {
+		result = "undefined";
+	} else {
+		result += "" + o;
+	}
+	return result;
+});
+
 while (true) {
 	var NHqK6i6xWR$pT25xXVV6K2xzFEOcDGHTz7lwJy_Y9ymbIjETcWxli$sPxAuJ_uS;
 	WScript.StdOut.Write(":> ");
@@ -9,7 +38,7 @@ while (true) {
 	}
 	WScript.StdOut.Write("<: ");
 	try {
-		WScript.Echo(eval(NHqK6i6xWR$pT25xXVV6K2xzFEOcDGHTz7lwJy_Y9ymbIjETcWxli$sPxAuJ_uS));
+		WScript.Echo(FV0IEh__qo76togShNc4ExIPkaagButmM17zwtGKNmh7Wjf$7lyJl$g7WGFuC0L(eval(NHqK6i6xWR$pT25xXVV6K2xzFEOcDGHTz7lwJy_Y9ymbIjETcWxli$sPxAuJ_uS)));
 	} catch (e) {
 		WScript.Echo("Eval Error " + e.message);
 		continue;
